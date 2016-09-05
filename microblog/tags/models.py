b5f0cd68ab6cc6eed844a6969ac9ca42ac4dfb88
lukas346 <lukasz.klim92@gmail.com>
2016-09-05 21:06:13 +0200

    Create tag model

--- microblog/tags/models.py
+++ microblog/tags/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50, verbose_name=_('Title'))
+    slug = models.CharField(max_length=50, verbose_name=_('Slug'))
+    created_by = models.ForeignKey(User, verbose_name=_('Created by'))
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = _('Tag')
+        verbose_name_plural = _('Tags')
+
+    def __str__(self):
+        return self.title
