95567f27cadc859d83b47ef4816fa61b14d2420e
lukas346 <lukasz.klim92@gmail.com>
2016-09-05 22:53:05 +0200

    Add Article ListView with templates

--- microblog/articles/views.py
+++ microblog/articles/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Article
+
+
+class ArticleListView(ListView):
+    template_name = 'articles/article_list.html'
+    context_object_name = 'articles'
+    queryset = Article.objects.published()
