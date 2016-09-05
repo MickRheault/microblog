3c646684cb35aab8f2583d9ff93e552a852d98bf
lukas346 <lukasz.klim92@gmail.com>
2016-09-05 21:28:35 +0200

    Create TagAdmin andd register it

--- microblog/tags/admin.py
+++ microblog/tags/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tag
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_by', 'creation_date')
+
+admin.site.register(Tag, TagAdmin)
+
