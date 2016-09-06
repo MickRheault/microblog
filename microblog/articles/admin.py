7378350cf881bc1c570994d635d495f24205c393
lukas346 <lukasz.klim92@gmail.com>
2016-09-06 22:53:24 +0200

    Auto update publish date when publish is true

--- microblog/articles/admin.py
+++ microblog/articles/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
+from django.utils import timezone
 
 from .models import Article
 
 
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'creation_date',)
+
+    def save_model(self, request, obj, form, change):
+        if form.cleaned_data['publish'] is True:
+            obj.publish_date = timezone.now()
+        super().save_model(request, obj, form, change)
 
 admin.site.register(Article, ArticleAdmin)
