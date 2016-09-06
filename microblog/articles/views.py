fb030c1a85e547883917af5f6460e24db6eeb955
lukas346 <lukasz.klim92@gmail.com>
2016-09-07 00:38:55 +0200

    Qeuryset optimisation

--- microblog/articles/views.py
+++ microblog/articles/views.py
@@ -7,4 +7,8 @@ from .models import Article
 class ArticleListView(ListView):
     template_name = 'articles/article_list.html'
     context_object_name = 'articles'
-    queryset = Article.objects.published()
+
+    def get_queryset(self):
+        queryset = Article.objects.published().select_related('author')
+        queryset = queryset.prefetch_related('tags')
+        return queryset
