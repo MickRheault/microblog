e483a3c6ec841506492debbb60e6f98dac0a3147
lukas346 <lukasz.klim92@gmail.com>
2016-09-06 22:57:36 +0200

    Add comment

--- microblog/articles/admin.py
+++ microblog/articles/admin.py
@@ -8,6 +8,7 @@ class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'creation_date',)
 
     def save_model(self, request, obj, form, change):
+        # Auto update publish date when publish is true
         if form.cleaned_data['publish'] is True:
             obj.publish_date = timezone.now()
         super().save_model(request, obj, form, change)
