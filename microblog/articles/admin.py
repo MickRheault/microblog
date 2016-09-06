b067252d371caabec9b8b94626d9f00bbdd89e35
lukas346 <lukasz.klim92@gmail.com>
2016-09-06 23:11:02 +0200

    Setup fields in ModelAdmin

--- microblog/articles/admin.py
+++ microblog/articles/admin.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 from .models import Article
 
 
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'creation_date',)
+    fields = ('title', 'slug', 'text', 'image', 'publish', 'tags')
 
     def save_model(self, request, obj, form, change):
+        obj.author = request.user
+
         # Auto update publish date when publish is true
         if form.cleaned_data['publish'] is True:
             obj.publish_date = timezone.now()
