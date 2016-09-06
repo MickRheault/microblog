a78e743d71c4477981ae63134d893214564ff61b
lukas346 <lukasz.klim92@gmail.com>
2016-09-07 01:35:58 +0200

    Add Article detail view along with ArticleMixin

--- microblog/articles/views.py
+++ microblog/articles/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Article
 
 
-class ArticleListView(ListView):
-    template_name = 'articles/article_list.html'
-    context_object_name = 'articles'
+class ArticleMixin(object):
 
-    def get_queryset(self):
+    @staticmethod
+    def get_queryset():
         queryset = Article.objects.published().select_related('author')
         queryset = queryset.prefetch_related('tags')
         return queryset
+
+
+class ArticleListView(ArticleMixin, ListView):
+    template_name = 'articles/article_list.html'
+    context_object_name = 'articles'
+
+
+class ArticleDetailView(ArticleMixin, DetailView):
+    template_name = 'articles/article_detail.html'
+    context_object_name = 'article'
