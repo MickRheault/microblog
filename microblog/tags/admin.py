1bce36a4b0f0dd6e4344ba035e07edd4fd1954ed
lukas346 <lukasz.klim92@gmail.com>
2016-09-12 20:03:12 +0200

    Autoadd author of tag

--- microblog/tags/admin.py
+++ microblog/tags/admin.py
@@ -5,7 +5,13 @@ from .models import Tag
 
 class TagAdmin(admin.ModelAdmin):
     list_display = ('title', 'created_by', 'creation_date')
+    fields = ('title', 'slug',)
     prepopulated_fields = {'slug': ('title',)}
+
+    def save_model(self, request, obj, form, change):
+        obj.author = request.user
+
+        super().save_model(request, obj, form, change)
 
 admin.site.register(Tag, TagAdmin)
 
