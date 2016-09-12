83472ad24a2bce0a678f5be8f800ee76cc171867
lukas346 <lukasz.klim92@gmail.com>
2016-09-12 19:36:02 +0200

    Make slug and title unique

--- microblog/tags/models.py
+++ microblog/tags/models.py
@@ -4,8 +4,8 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class Tag(models.Model):
-    title = models.CharField(max_length=50, verbose_name=_('Title'))
-    slug = models.CharField(max_length=50, verbose_name=_('Slug'))
+    title = models.CharField(max_length=50, verbose_name=_('Title'), unique=True)
+    slug = models.CharField(max_length=50, verbose_name=_('Slug'), unique=True)
     created_by = models.ForeignKey(User, verbose_name=_('Created by'))
     creation_date = models.DateTimeField(auto_now_add=True)
 
