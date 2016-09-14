32b7168af8f6debf6ccc56b731ca0b810332feac
lukas346 <lukasz.klim92@gmail.com>
2016-09-15 00:18:10 +0200

    Slugify title and use it as object slug

--- microblog/articles/admin.py
+++ microblog/articles/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.utils import timezone
+from django.utils.text import slugify
 
 from .models import Article
 
 
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'creation_date',)
-    fields = ('title', 'slug', 'text', 'image', 'publish', 'tags')
-    prepopulated_fields = {'slug': ('title',)}
+    fields = ('title', 'text', 'image', 'publish', 'tags')
 
     def save_model(self, request, obj, form, change):
         obj.author = request.user
@@ -15,6 +15,10 @@ class ArticleAdmin(admin.ModelAdmin):
         # Auto update publish date when publish is true
         if form.cleaned_data['publish'] is True:
             obj.publish_date = timezone.now()
+
+        # Slugify title and save it as slug
+        obj.slug = slugify(form.cleaned_data['title'])
+
         super().save_model(request, obj, form, change)
 
 admin.site.register(Article, ArticleAdmin)
--- microblog/other/admin.py
+++ microblog/other/admin.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
+from django.utils.text import slugify
 
 from .models import Other
 
 
 class OtherAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'creation_date',)
-    fields = ('title', 'slug', 'text', 'image')
-    prepopulated_fields = {'slug': ('title',)}
+    fields = ('title', 'text', 'image')
 
     def save_model(self, request, obj, form, change):
         obj.author = request.user
+
+        # Slugify title and save it as slug
+        obj.slug = slugify(form.cleaned_data['title'])
+
         super().save_model(request, obj, form, change)
 
 admin.site.register(Other, OtherAdmin)
