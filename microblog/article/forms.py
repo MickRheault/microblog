98b3430af7f67c9bebb572ef1e76d220aaadf732
lukas346 <lukasz.klim92@gmail.com>
2016-09-20 20:18:22 +0200

    Improve searchbar look

--- microblog/article/forms.py
+++ microblog/article/forms.py
@@ -3,11 +3,6 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class SearchForm(forms.Form):
-    search = forms.CharField(max_length=30)
-
-    class Meta:
-        widgets = {
-            'search': forms.TextInput(
-                attrs={'placeholder': _('Search'), 'class': 'form-control'}
-            )
-        }
+    search = forms.CharField(max_length=30, widget=forms.TextInput(
+        attrs={'placeholder': _('Search'), 'class': 'form-control'}
+    ))
