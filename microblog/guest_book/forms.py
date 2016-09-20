95795150bfc1a369ead8fe35e7105ca939a307eb
lukas346 <lukasz.klim92@gmail.com>
2016-09-20 21:49:09 +0200

    Author field is now required

--- microblog/guest_book/forms.py
+++ microblog/guest_book/forms.py
@@ -13,7 +13,7 @@ class EntryForm(forms.ModelForm):
         model = GuessBookEntry
         widgets = {
             'author': forms.TextInput(
-                attrs={'class': 'form-control', 'placeholder': 'Type your name...'}
+                attrs={'required': True, 'class': 'form-control', 'placeholder': 'Type your name...'}
             ),
             'text' : forms.Textarea(
                 attrs={'required': True, 'placeholder': 'Say something...', 'class': 'form-control', 'rows': '3'}
