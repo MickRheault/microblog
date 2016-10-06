e6180b2becc361481e26687ccb9e557daed1c87f
lukas346 <lukasz.klim92@gmail.com>
2016-10-06 12:22:11 +0200

    Add Article Feed

--- microblog/article/urls.py
+++ microblog/article/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
-from .views import ArticleListView, ArticleDetailView, ArticleAuthorListView
+from .views import ArticleListView, ArticleDetailView, ArticleAuthorListView, LatestArticlesFeed
 
 urlpatterns = [
     url(r'^$', ArticleListView.as_view(), name='list'),
+    url(r'latest/feed/$', LatestArticlesFeed()),
     url(r'author/(?P<slug>\w+)/$', ArticleAuthorListView.as_view(), name='author-list'),
     url(r'(?P<slug>[\w-]+)/$', ArticleDetailView.as_view(), name='detail'),
 ]
