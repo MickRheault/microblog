35971a306397d10ea83c3f42a3fe647942201a3c
lukas346 <lukasz.klim92@gmail.com>
2017-03-11 21:32:39 +0100

    About me is text field (person model)

--- microblog/account/models.py
+++ microblog/account/models.py
@@ -5,7 +5,7 @@ from core.utils import avatar_directory_path
 
 class User(AbstractUser):
     avatar = models.ImageField(verbose_name='Awatar', upload_to=avatar_directory_path, blank=True)
-    about_me = models.CharField(verbose_name='O mnie', max_length=255, blank=True)
+    about_me = models.TextField(verbose_name='O mnie', blank=True)
 
     class Meta:
         verbose_name = 'Użytkownik'
