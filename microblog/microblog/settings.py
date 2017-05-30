7bbdec74b3fe4509a52a7627a5d78cf284dc69df
lukas346 <lukasz.klim92@gmail.com>
2017-05-30 11:10:23 +0200

    Update version

--- microblog/microblog/settings.py
+++ microblog/microblog/settings.py
@@ -141,7 +141,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 PROHIBITED_NAMES = ['author', 'tag', 'guest_book', 'other', 'file', 'api', 'lang']
 
 # Project version
-VERSION = '0.8.6'
+VERSION = '0.8.7'
 
 # Rest config
 REST_FRAMEWORK = {
