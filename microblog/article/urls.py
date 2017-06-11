52b9afbae4f10d138c190f83636e941752f829ba
lukas346 <lukasz.klim92@gmail.com>
2017-06-11 23:06:58 +0200

    Disable cache on main page

--- microblog/article/urls.py
+++ microblog/article/urls.py
@@ -7,7 +7,7 @@ from .views import ArticleListView, ArticleDetailView, ArticleAuthorListView, La
 
 urlpatterns = [
     url(r'preview/(?P<pk>\d+)/$', ArticlePreviewView.as_view(), name='preview'),
-    url(r'^$', cache_page(settings.CACHE_TIME)(ArticleListView.as_view()), name='list'),
+    url(r'^$', ArticleListView.as_view(), name='list'),
     url(r'latest/feed/$', LatestArticlesFeed()),
     url(r'author/(?P<slug>\w+)/$', cache_page(settings.CACHE_TIME)(ArticleAuthorListView.as_view()),
         name='author-list'),
