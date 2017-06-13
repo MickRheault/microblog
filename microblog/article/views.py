5249a038af701d5dfa5d1a139c6ff5858f9fda59
lukas346 <lukasz.klim92@gmail.com>
2017-06-13 14:37:28 +0200

    Paginate by 7

--- microblog/article/views.py
+++ microblog/article/views.py
@@ -19,7 +19,7 @@ class ArticleMixin(object):
 class ArticleListView(ArticleMixin, ListView):
     template_name = 'articles/article_list.html'
     context_object_name = 'articles'
-    paginate_by = 5
+    paginate_by = 7
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -59,7 +59,7 @@ class ArticlePreviewView(DetailView):
 class ArticleAuthorListView(ArticleMixin, ListView):
     template_name = 'articles/article_list.html'
     context_object_name = 'articles'
-    paginate_by = 5
+    paginate_by = 7
 
     def get_queryset(self):
         slug = self.kwargs.get('slug')
@@ -95,7 +95,7 @@ class LatestArticlesFeed(Feed):
 class SearchView(ArticleMixin, ListView):
     template_name = 'articles/article_list.html'
     context_object_name = 'articles'
-    paginate_by = 5
+    paginate_by = 7
 
     def get_queryset(self):
         queryset = super().get_queryset()
