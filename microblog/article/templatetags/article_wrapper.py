3176afc40fc8e3322523f4897ce9c894cade4505
lukas346 <lukasz.klim92@gmail.com>
2017-07-02 14:21:00 +0200

    Improve images in article details

--- microblog/article/templatetags/article_wrapper.py
+++ microblog/article/templatetags/article_wrapper.py
@@ -6,7 +6,7 @@ register = template.Library()
 
 @register.filter(name="wrap_img")
 def markdownify(content):
-    pattern = r'(<img.*src="(.*)".*/>)'
-    to = r'<a href="\2" class="pic">\1</a>'
+    pattern = r'<img.*src="(.*)".*/>'
+    to = r'<a href="\1" class="pic"><img src="\1" class="img-responsive center-block"/></a>'
 
     return sub(pattern, to, content)
