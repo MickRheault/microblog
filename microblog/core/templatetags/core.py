5e7e7c35ca28b144d49fbe4fc39d61fa874fe190
lukas346 <lukasz.klim92@gmail.com>
2017-07-05 19:59:47 +0200

    Add img center filter and use it in newsletter raport

--- microblog/core/templatetags/core.py
+++ microblog/core/templatetags/core.py
@@ -1,5 +1,6 @@
 from django import template
 from django.conf import settings
+from re import sub
 
 register = template.Library()
 
@@ -7,3 +8,10 @@ register = template.Library()
 @register.simple_tag
 def version():
     return settings.VERSION
+
+@register.filter(name="center_img")
+def img(content):
+    pattern = r'<img.*src="(.*)".*/>'
+    to = r'<img src="\1" class="img-responsive center-block">'
+
+    return sub(pattern, to, content)
